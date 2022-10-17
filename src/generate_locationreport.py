519bd560c47709e1d19b2bf2eed2f4289b890e86
NikolaiVestbostad <nikolv@stud.ntnu.no>
2022-10-17 14:52:25 +0200

    Update meeting log

---
+++ src/generate_locationreport.py
@@ -0,0 +1,38 @@
+#Generate The headers of the new dataset (width 12)
+listTitles = ["LocationName",
+"LocationNumber",
+"Owner",
+"ProductionAreaNumber",
+"Municipality",
+"MaxBiomassLicence",
+"MaxBiomassLocation",
+"InstaceType",
+"SedimentationLevel",
+"Licecount",
+"SedimentationAlarmLevel",
+"LiceAlarmLevel",
+"IsFish"]
+
+# We need to write the fetching code for each of these, some will need to be done for every single new line, others will be done for each new location
+"""
+LocationName = 
+LocationNumber = 
+Owner = 
+ProductionAreaNumber = 
+Municipality = 
+MaxBiomassLicence =
+MaxBiomassLocation =
+InstanceType = 
+SedimentationLevel = 
+Licecount = 
+SedimentationAlarmLevel =
+LiceAlarmLevel = 
+IsFish
+"""
+
+# Pseudocode completion 
+
+# function that makes a pandas dataframe based on a locationnumber 
+"""def generateDataframe(locationNumber) -
+"""
+
