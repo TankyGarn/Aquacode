ce21847f787141b3db6376ae1c0639e4c47d6f92
NikolaiVestbostad <nikolv@stud.ntnu.no>
2022-10-18 11:03:48 +0200

    creating the list stuff.

--- src/generate_locationreport.py
+++ src/generate_locationreport.py
@@ -1,3 +1,7 @@
+import pandas as pd
+
+sl = pd.read_csv("../data/interim/sorted_licence.csv")
+
 #Generate The headers of the new dataset (width 12)
 listTitles = ["LocationName",
 "LocationNumber",
@@ -21,7 +25,28 @@ listTitles = ["LocationName",
 "SealiceLimit"
 ]
 
+
 # We need to write the fetching code for each of these, some will need to be done for every single new line, others will be done for each new location
+
+# Lets begin with the one time to storage data
+
+
+#Can we create a object to do this????? (since it is just repetition of data with one swap on every new collumb name)
+def getLocationName(LocationID):
+    #Checkup if there is a location number and return a df with every single location with that id
+    checkup = sl[sl["LOK_NR"]==LocationID]
+    #Return the first Location number in that list.
+    return(checkup["LOK_NAVN"][checkup.index[0]])
+
+def getOwner(LocationID):
+    checkup = sl[sl["NAVN"]==LocationID]
+    return(checkup["NAVN"][checkup.index[0]])
+
+def getLatitude(LocationID):
+    checkup = sl[sl["N_GEOWGS84"]==LocationID]
+    return(checkup["N_GEOWGS84"][checkup.index[0]])
+
+
 """
 LocationName = 
 LocationNumber = 
@@ -48,6 +73,12 @@ SealiceLimit =
 # Pseudocode completion 
 
 # function that makes a pandas dataframe based on a locationnumber 
-"""def generateDataframe(locationNumber) -
-"""
+def generateDataList(LocationID):
+    dataList = []
+    dataList.append(getLocationName(LocationID))
 
+    print(dataList)
+    return dataList
+
+
+generateDataList(10256)
