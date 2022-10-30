1c5a60fe224b3b9ee434d1c0f8fd49e21a2bb6ff
NikolaiVestbostad <nikolv@stud.ntnu.no>
2022-10-30 22:08:51 +0100

    test

--- src/generate_locationreport.py
+++ src/generate_locationreport.py
@@ -2,15 +2,26 @@ import pandas as pd
 
 df_sortedLicence = pd.read_csv("../data/interim/sorted_licence.csv")
 df_sortedLice = pd.read_csv("../data/interim/sorted_lice.csv")
+df_sortedLicence.rename(columns = {'LOK_NR':'Loction_Number', 'LOK_NAVN':'Location_Name', 'TILL_NR':'Licence_Id', 'ORG-NR/PERS.NR':'Buisness_Id', 'NAVN':'Buisness_Name', 'Adresse':'Adress', 'POSTNR':'Area_Code', 'POSTSTED':'Area_Name','TILDELINGSPUNKT':'Licence_Startdate', 'LOK_KOMNR':'Municipality_ID','LOK_KOM':'Municipality_Name', 'LOK_KAP':'Municipality_Capacity',    }, inplace = True)
 
 # A function that takes input location ID and ColumbName (Index of collumb of interest)
 def returnColumbData(LocationID,ColumbName,columbIndex):
     checkup = df_sortedLicence[df_sortedLicence["LOK_NR"]==LocationID]
     return (checkup[ColumbName][checkup.index[columbIndex]])
+# not sure if usefull.
+def returnColumbDataLice(LocationID,ColumbName,columbIndex):
+    checkup = df_sortedLice[df_sortedLice["Lokalitetsnummer"]==LocationID]
+    return (checkup[ColumbName][checkup.index[columbIndex]])
 
+df_samlet = pd.merge(df_sortedLicence,df_sortedLice)
+print(df_sortedLice)
+print(df_sortedLicence)
+print(df_samlet)
 
+df_sortedLice.to_csv("../data/interim/combined_raw_licence_lice.csv")
 
-TitleDictOnetimeLicence= {
+"""
+DictFromLicence={
     "Location_Name" : "LOK_NAVN",
     "Location_Owner" : "NAVN",
     "Municipalty_Name" : "TILL_KOM",
@@ -21,7 +32,7 @@ TitleDictOnetimeLicence= {
     "Longitude" : "Ø_GEOWGS84"
     
 }
-TitleDictEveryTime={
+DictFromLice={
     "Instancetype" : "INSTANCETYPE",
     "Location_Name" :"Lokalitetsnavn",
     "Adult_Female_Lice" : "Voksne hunnlus",
@@ -32,6 +43,7 @@ TitleDictEveryTime={
 # add the datalist together with the new list
 # Create a list of the 2. element in each of the rows.
 
+keysList = list(DictFromLicence.keys())+list(DictFromLice.keys())
 
 #Generate The headers of the new dataset (width 12)
 listTitrttles = ["LocationName",
@@ -65,16 +77,25 @@ listTitrttles = ["LocationName",
 # Pseudocode completion 
 dataList = []
 # function that makes a pandas dataframe based on a locationnumber 
-mainList = []
+mainList = pd.DataFrame(columns=keysList)
 
 def appendOneTimeDataToList(LocationID):
-    for key in TitleDictOnetimeLicence:
-        dataList.append(returnColumbData(LocationID,TitleDictOnetimeLicence[key],0))
+    for key in DictFromLicence:
+        dataList.append(returnColumbData(LocationID,DictFromLicence[key],0))
     return dataList
-    
 
+def LappendOneTimeDataToList(LocationID):
+    for key in DictFromLice:
+        dataList.append(returnColumbDataLice(LocationID,DictFromLice[key],0))
+    return dataList
    
-        
+def addLocationDataToMainList(LocationID):
+    LappendOneTimeDataToList(LocationID)
+    appendOneTimeDataToList(LocationID)
+    mainList.append(dataList)
 
+addLocationDataToMainList(30096)
 
-print(appendOneTimeDataToList(15196))
+print(mainList)
+
+"""
