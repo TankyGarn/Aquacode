f5cf676baae1404a040e6ac16314de19f6e7906d
NikolaiVestbostad <nikolv@stud.ntnu.no>
2022-10-31 10:59:17 +0100

    redo the bad stuff from earlier

--- src/generate_locationreport.py
+++ src/generate_locationreport.py
@@ -4,6 +4,19 @@ df_sortedLicence = pd.read_csv("../data/interim/sorted_licence.csv")
 df_sortedLice = pd.read_csv("../data/interim/sorted_lice.csv")
 df_sortedLicence.rename(columns = {'LOK_NR':'Lokalitetsnummer', 'LOK_NAVN':'Location_Name', 'TILL_NR':'Licence_Id', 'ORG-NR/PERS.NR':'Buisness_Id', 'NAVN':'Buisness_Name', 'Adresse':'Adress', 'POSTNR':'Area_Code', 'POSTSTED':'Area_Name','TILDELINGSPUNKT':'Licence_Startdate', 'LOK_KOMNR':'Municipality_ID','LOK_KOM':'Municipality_Name', 'LOK_KAP':'Municipality_Capacity',    }, inplace = True)
 
+
+df_samlet = pd.merge(df_sortedLicence,df_sortedLice)
+
+print(df_samlet)
+
+df_samlet.to_csv("../data/interim/combined_raw_licence_lice.csv")
+print("/combined_raw_licence_lice.csv is generated." )
+#The code below might be usefull later.
+
+
+"""
+
+
 # A function that takes input location ID and ColumbName (Index of collumb of interest)
 def returnColumbData(LocationID,ColumbName,columbIndex):
     checkup = df_sortedLicence[df_sortedLicence["LOK_NR"]==LocationID]
@@ -12,15 +25,7 @@ def returnColumbData(LocationID,ColumbName,columbIndex):
 def returnColumbDataLice(LocationID,ColumbName,columbIndex):
     checkup = df_sortedLice[df_sortedLice["Lokalitetsnummer"]==LocationID]
     return (checkup[ColumbName][checkup.index[columbIndex]])
-
-df_samlet = pd.merge(df_sortedLicence,df_sortedLice)
-print(df_sortedLice)
-print(df_sortedLicence)
-print(df_samlet)
-
-df_sortedLice.to_csv("../data/interim/combined_raw_licence_lice.csv")
-
-"""
+    
 DictFromLicence={
     "Location_Name" : "LOK_NAVN",
     "Location_Owner" : "NAVN",
