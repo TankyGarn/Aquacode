bab75a58a06f8f9404476c628e0e022bec6ff959
NikolaiVestbostad <nikolv@stud.ntnu.no>
2022-11-09 11:17:58 +0100

    update some stuff

--- src/03_generate_locationreport.py
+++ src/03_generate_locationreport.py
@@ -1,24 +1,9 @@
 import pandas as pd
 
-df_sortedLicence = pd.read_csv("../data/interim/sorted_licence.csv")
-df_sortedLice = pd.read_csv("../data/interim/sorted_lice.csv")
-df_sortedLicence.rename(
-    columns={
-        "LOK_NR": "Lokalitetsnummer",
-        "LOK_NAVN": "Location_Name",
-        "TILL_NR": "Licence_Id",
-        "ORG-NR/PERS.NR": "Buisness_Id",
-        "NAVN": "Buisness_Name",
-        "Adresse": "Adress",
-        "POSTNR": "Area_Code",
-        "POSTSTED": "Area_Name",
-        "TILDELINGSPUNKT": "Licence_Startdate",
-        "LOK_KOMNR": "Municipality_ID",
-        "LOK_KOM": "Municipality_Name",
-        "LOK_KAP": "Municipality_Capacity",
-    },
-    inplace=True,
-)
+data_path = directory_path / ".." / "data"
+raw_path = data_path / "raw" / "FDIR"
+own_path = data_path / "raw" / "OWN"
+interim_path = data_path / "interim"
 
 
 df_samlet = pd.merge(df_sortedLicence, df_sortedLice)
